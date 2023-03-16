953c9d5ffc85cf541c89336ea8d18f883ba9ddbd
DARKADOFAI <128044658+DARKADOFAI@users.noreply.github.com>
2023-03-16 18:38:27 +0300

    Add files via upload

---
+++ DOORS TABLE.lua
@@ -0,0 +1,6 @@
+_G.UpdateStars = false -- stars disappear after picking up a book/breaker pole | false: a little lagцвфы
+_G.OnShop = true -- can buy on pre run shop
+_G.Price = 10 -- tablet price on shop
+_G.Description = "by DARKADOFAI" -- tablet description on shop
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DeividComSono/Scripts/main/Scanner.lua'))()
